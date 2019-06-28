f70751612a281e8a72346cf6746dab5a82ae59b7
Paulo Marcio <paulom.matias@gmail.com>
2019-06-28 17:59:34 +0000

    settings.py alterados online com o Bitbucket

--- settings.py
+++ settings.py
@@ -1,22 +1,18 @@
 # XTech API Settings
-endpoint = "toyboy1.xtechcommerce.com"
-api_key = "31689d04d0f2add137935769559adfff"
-app_key = "TH4GEnAtreStutHU6h73UxaDrayUyA"
-
-#endpoint = "staging.ambienteprotegido.com"
-#api_key = "f36bd8ea59a790c3c60ca85063d94fa2"
-#app_key = "NlNWbQkwEqaTHvVahExKCamzVXHTFn"
+endpoint = ""
+api_key = ""
+app_key = ""
 
 # Local database settings
-db_host = "127.0.0.1"
-db_user = "root"
-db_password = "root"
-db_name = "toyboy"
+db_host = ""
+db_user = ""
+db_password = ""
+db_name = ""
 db_products = 3210
 db_per_page = 2
 db_start_page = 1
 
 # S3 Settings
-s3_bucket_url = 'https://s3-sa-east-1.amazonaws.com/toyboy'
-s3_bucket_name = 'toyboy'
-s3_bucket_key = 'AKIAJZ36H3UDZWOOWC2Q'
+s3_bucket_url = ''
+s3_bucket_name = ''
+s3_bucket_key = ''
